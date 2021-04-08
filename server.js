17e01aaca5769b72e8dd7c8fde24677240a0dc28
Illya Havrulyk <peoso595@gmail.com>
2021-04-08 20:13:35 +0300

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 //Initializing application.
 const app = express();
 
@@ -15,9 +16,12 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-//Reacting on a simple endpoint just to ensure that the app works properly.
-app.get('/', (req, res) => res.send("Api running..."));
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 //Port setting and app starting.
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => `Server started on port ${PORT}`);
